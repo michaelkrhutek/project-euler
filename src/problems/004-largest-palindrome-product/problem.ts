160b5b6dac4617a0d968920b2b432a729aa992ca
Michael Krhůtek <michaelkrhutek@Michaels-Air.dkm.cz>
2022-02-01 22:45:31 +0100

    problem 4 - template

---
+++ src/problems/004-largest-palindrome-product/problem.ts
@@ -0,0 +1,11 @@
+// Problem 4: Largest palindrome product
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+// Find the largest palindrome made from the product of two n-digit numbers.
+
+const largestPalindromeProduct = (number: number): number => {
+  // TODO
+  return 0;
+};
+
+export default largestPalindromeProduct;
